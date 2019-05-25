2d22c255cd405feab8b80240892ba50f0bf6ce16
James Livolsi <james.livolsi08@gmail.com>
2019-05-25 16:54:51 -0400

    back to 15 minutes

--- dbWatcher.js
+++ dbWatcher.js
@@ -45,7 +45,7 @@ const watchPendingFunds = () => {
             )
             if (fundDone && !returning) {
               returning = true
-              manager.scheduleFundReturning(fund.id, Date.now()) //+ 15 * 60 * 1000)
+              manager.scheduleFundReturning(fund.id, Date.now() + 15 * 60 * 1000)
             }
           })
       })
