29d3353e8657e0515ac93bf46c95ac42f0f7083c
James Livolsi <james.livolsi08@gmail.com>
2019-05-29 21:16:46 -0400

    add env settings

--- firebase.js
+++ firebase.js
@@ -3,11 +3,17 @@
 const admin = require('firebase-admin')
 const config = require('./config.js')
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCT)
+const serviceAccount = process.env.BACKEND_ENV === 'debug'
+  ? JSON.parse(process.env.FIREBASE_DEBUG_SERVICE_ACCT)
+  : JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCT)
 
-const databaseURL = config.firebase.databaseURL
+const databaseURL = process.env.BACKEND_ENV === 'debug'
+  ? config.staging.databaseURL
+  : config.prod.databaseURL
 
-const storageBucket = config.firebase.storageBucket
+const storageBucket = process.env.BACKEND_ENV === 'debug'
+  ? config.staging.storageBucket
+  : config.prod.storageBucket
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
