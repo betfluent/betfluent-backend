0dc32ba547691f596c00de78ed6772407afb4f4c
James Livolsi <james.livolsi08@gmail.com>
2019-05-30 11:54:44 -0400

    change to live

--- services/PayPalService.js
+++ services/PayPalService.js
@@ -3,7 +3,7 @@ const paypal = require('@paypal/checkout-server-sdk')
 // Creating an environment
 let clientId = process.env.PAYPAL_CLIENT
 let clientSecret = process.env.PAYPAL_KEY
-let environment = process.env.PAYPAL_MODE === 'LIVE' ? new paypal.core.ProductionEnvironment(clientId, clientSecret) : new paypal.core.SandboxEnvironment(clientId, clientSecret)
+let environment = process.env.PAYPAL_MODE === 'LIVE' ? new paypal.core.LiveEnvironment(clientId, clientSecret) : new paypal.core.SandboxEnvironment(clientId, clientSecret)
 let client = new paypal.core.PayPalHttpClient(environment)
 
 const RetrieveOrder = async (orderId) => {
